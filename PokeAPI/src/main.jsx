3ffb07f84838272dfe218e9710acfdf77fe58d60
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-10-02 21:19:43 -0400

    Put in dark mode theme from MaterialUI

--- PokeAPI/src/main.jsx
+++ PokeAPI/src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 )
+
+// Here ^ we just wrapped the app with the materialUI theme component and set the theme to dark. Now our entire app will be dark themed since our app is wrapped INSIDE the dark theme component.
